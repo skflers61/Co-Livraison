afa09a54f763596e12a4c0d9023a9308ed172101
skflers61 <skflers61@gmail.com>
2022-04-12 15:43:54 +0200

    accueil v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "./container/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
+      <Container fluid>
+        <Row></Row>
+        <Row></Row>
+        <Row>
+          <Col></Col>
+          <Col>
+            <div className="App">
+              <h1>Hello, React!</h1>
+              <Header />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
