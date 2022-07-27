6e72105372e32707b60a2eee19c7c8e1cb63fab9
skflers61 <skflers61@gmail.com>
2022-07-27 16:06:48 +0200

    déplacer le slice redux dans un dossier

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { store } from "./redux";
+import { store } from "./redux-slices/vue";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
